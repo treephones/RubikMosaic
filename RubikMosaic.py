50cf21d565b01a77a6b1d983a8a1028cd916607b
Moez B <moezbajwa@hotmail.com>
2021-06-04 23:44:56 -0400

    random thingaz

--- RubikMosaic.py
+++ RubikMosaic.py
@@ -1,3 +1,2 @@
-from process.ImageParser import prepare_image, Colors, Color
-
-print(prepare_image("C:/Users/moezb/OneDrive/Desktop/Python Projects/RubikMosaic/statics/test.jpg", 5000))
+#Rubik Mosaic
+#Vic Tong
